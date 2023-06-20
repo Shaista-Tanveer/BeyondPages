17c6e88f80cc072b0c4eecd8aa55b0ac54b9515a
loadingthedev <loadingthedev@gmail.com>
2023-06-20 15:02:33 +0530

    search in blogs

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,13 +1,15 @@
-import { connectToDB } from '../../../utils/database';
-import Blog from '../../../models/Blogs';
-
+import { connectToDB } from '../../../utils/database'
+import Blog from '../../../models/Blogs'
 
 export const GET = async (request) => {
-    try {
-        await connectToDB();
-        const blogs = await Blog.find();
-        return new Response(JSON.stringify(blogs), { status: 200 });
-    } catch (error) {
-        return new Response("Failed to create a new prompt", { status: 500 });
-    }
+  try {
+    const title = request.nextUrl.searchParams.get('title')
+    await connectToDB()
+    const blogs = await Blog.find(
+      title ? { title: { $regex: title, $options: 'i' } } : {}
+    )
+    return new Response(JSON.stringify(blogs), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to create a new prompt', { status: 500 })
+  }
 }
